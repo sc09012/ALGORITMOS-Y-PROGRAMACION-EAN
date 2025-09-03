88818fa6c9b859025b00190bf3445625176517df
sc09012 <114302161+sc09012@users.noreply.github.com>
2025-09-03 07:59:01 -0500

    Update main.py
    v1

---
+++ proyecto Hotel/main.py
@@ -0,0 +1,22 @@
+# main.py
+
+import receptionist_module
+import administrator_module
+
+def main():
+    while True:
+        print("\n--- Sistema de Gestión de Hotel ---")
+        rol = input("¿Eres administrador o recepcionista? (a/r/salir): ").lower()
+        
+        if rol == 'a':
+            administrator_module.login()
+        elif rol == 'r':
+            receptionist_module.login()
+        elif rol == 'salir':
+            print("Saliendo del programa.")
+            break
+        else:
+            print("Opción no válida. Intenta de nuevo.")
+
+if __name__ == "__main__":
+    main()
