a231d077bb77d86ed8c0d7f9a6b3ddaea201523e
sc09012 <114302161+sc09012@users.noreply.github.com>
2025-09-19 08:35:50 -0500

    Update main.py

--- proyecto Hotel/main.py
+++ proyecto Hotel/main.py
@@ -4,19 +4,19 @@ import receptionist_module
 import administrator_module
 
 def main():
-    while True:
-        print("\n--- Sistema de Gestión de Hotel ---")
-        rol = input("¿Eres administrador o recepcionista? (a/r/salir): ").lower()
-        
-        if rol == 'a':
-            administrator_module.login()
-        elif rol == 'r':
-            receptionist_module.login()
-        elif rol == 'salir':
-            print("Saliendo del programa.")
-            break
-        else:
-            print("Opción no válida. Intenta de nuevo.")
+while True:
+print("\n--- Sistema de Gestión de Hotel ---")
+rol = input("¿Eres administrador o recepcionista? (a/r/salir): ").lower()
+
+if rol == 'a':
+administrator_module.login()
+elif rol == 'r':
+receptionist_module.login()
+elif rol == 'salir':
+print("Saliendo del programa.")
+break
+else:
+print("Opción no válida. Intenta de nuevo.")
 
 if __name__ == "__main__":
-    main()
+main()
